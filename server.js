6757b1efb70a74d4f65ab5641b4ebf221d59dc5e
JPKilburn <jpkilburn1@gmail.com>
2015-11-20 18:33:01 -0600

    Added a front-end.
    The program can now be installed and ran. All of the content is likely
    placeholder right now. Added some more structure and added EJS as a
    dependency in the package file.

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
 'use strict';
+
+// set up ==========================================================
+var express = require('express');
+var app			= express();
+var port		= process.env.PORT || 8012;
+
+// configuration ===================================================
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
+// pages
+app.get('/', function(req, res) {
+    res.render('pages/index', {title:'Index Page'});
+});
+app.get('/characters', function(req, res) {
+    res.render('pages/characters', {title:'Character List'});
+});
+app.get('/items', function(req, res) {
+    res.render('pages/items', {title:'Item List'});
+});
+app.get('/locations', function(req, res) {
+    res.render('pages/locations', {title:'Location List'});
+});
+
+// listen ==========================================================
+app.listen(port);
+console.log("App listening on port " + port);
