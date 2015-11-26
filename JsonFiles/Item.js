a9920e5156b17287bc6e6f9e0f66042a01d88ee8
Haverfist <dillonrobinson5@yahoo.com>
2015-11-25 21:08:59 -0600

    copies, items, and locations
    Have 20 copies of each made(but not filled out yet), along with 12
    different location names, and 18 item names. Will start to change each
    individual Json file in the next couple of days.

--- JsonFiles/Item.js
+++ JsonFiles/Item.js
@@ -10,3 +10,250 @@
 		price: 160;
 		...
 }
+
+{
+		_id: Object('...'),
+		name: '',
+		bonus: null,
+		inventory: [
+			{qty:4, id:ObjectId('...'), name: 'potion of healing'},
+			{qty:1, id:ObjectId('...'), name: 'scroll of magic mapping'},
+			{qty:2, id:ObjectId('...'), name: 'c-rations'} ]},	
+		weight: 12,
+		price: 160;
+		...
+}
+
+{
+		_id: Object('...'),
+		name: '',
+		bonus: null,
+		inventory: [
+			{qty:4, id:ObjectId('...'), name: 'potion of healing'},
+			{qty:1, id:ObjectId('...'), name: 'scroll of magic mapping'},
+			{qty:2, id:ObjectId('...'), name: 'c-rations'} ]},	
+		weight: 12,
+		price: 160;
+		...
+}
+
+{
+		_id: Object('...'),
+		name: '',
+		bonus: null,
+		inventory: [
+			{qty:4, id:ObjectId('...'), name: 'potion of healing'},
+			{qty:1, id:ObjectId('...'), name: 'scroll of magic mapping'},
+			{qty:2, id:ObjectId('...'), name: 'c-rations'} ]},	
+		weight: 12,
+		price: 160;
+		...
+}
+
+{
+		_id: Object('...'),
+		name: '',
+		bonus: null,
+		inventory: [
+			{qty:4, id:ObjectId('...'), name: 'potion of healing'},
+			{qty:1, id:ObjectId('...'), name: 'scroll of magic mapping'},
+			{qty:2, id:ObjectId('...'), name: 'c-rations'} ]},	
+		weight: 12,
+		price: 160;
+		...
+}
+
+{
+		_id: Object('...'),
+		name: '',
+		bonus: null,
+		inventory: [
+			{qty:4, id:ObjectId('...'), name: 'potion of healing'},
+			{qty:1, id:ObjectId('...'), name: 'scroll of magic mapping'},
+			{qty:2, id:ObjectId('...'), name: 'c-rations'} ]},	
+		weight: 12,
+		price: 160;
+		...
+}
+
+{
+		_id: Object('...'),
+		name: '',
+		bonus: null,
+		inventory: [
+			{qty:4, id:ObjectId('...'), name: 'potion of healing'},
+			{qty:1, id:ObjectId('...'), name: 'scroll of magic mapping'},
+			{qty:2, id:ObjectId('...'), name: 'c-rations'} ]},	
+		weight: 12,
+		price: 160;
+		...
+}
+
+{
+		_id: Object('...'),
+		name: '',
+		bonus: null,
+		inventory: [
+			{qty:4, id:ObjectId('...'), name: 'potion of healing'},
+			{qty:1, id:ObjectId('...'), name: 'scroll of magic mapping'},
+			{qty:2, id:ObjectId('...'), name: 'c-rations'} ]},	
+		weight: 12,
+		price: 160;
+		...
+}
+
+{
+		_id: Object('...'),
+		name: '',
+		bonus: null,
+		inventory: [
+			{qty:4, id:ObjectId('...'), name: 'potion of healing'},
+			{qty:1, id:ObjectId('...'), name: 'scroll of magic mapping'},
+			{qty:2, id:ObjectId('...'), name: 'c-rations'} ]},	
+		weight: 12,
+		price: 160;
+		...
+}
+
+{
+		_id: Object('...'),
+		name: '',
+		bonus: null,
+		inventory: [
+			{qty:4, id:ObjectId('...'), name: 'potion of healing'},
+			{qty:1, id:ObjectId('...'), name: 'scroll of magic mapping'},
+			{qty:2, id:ObjectId('...'), name: 'c-rations'} ]},	
+		weight: 12,
+		price: 160;
+		...
+}
+
+{
+		_id: Object('...'),
+		name: '',
+		bonus: null,
+		inventory: [
+			{qty:4, id:ObjectId('...'), name: 'potion of healing'},
+			{qty:1, id:ObjectId('...'), name: 'scroll of magic mapping'},
+			{qty:2, id:ObjectId('...'), name: 'c-rations'} ]},	
+		weight: 12,
+		price: 160;
+		...
+}
+
+{
+		_id: Object('...'),
+		name: '',
+		bonus: null,
+		inventory: [
+			{qty:4, id:ObjectId('...'), name: 'potion of healing'},
+			{qty:1, id:ObjectId('...'), name: 'scroll of magic mapping'},
+			{qty:2, id:ObjectId('...'), name: 'c-rations'} ]},	
+		weight: 12,
+		price: 160;
+		...
+}
+
+{
+		_id: Object('...'),
+		name: '',
+		bonus: null,
+		inventory: [
+			{qty:4, id:ObjectId('...'), name: 'potion of healing'},
+			{qty:1, id:ObjectId('...'), name: 'scroll of magic mapping'},
+			{qty:2, id:ObjectId('...'), name: 'c-rations'} ]},	
+		weight: 12,
+		price: 160;
+		...
+}
+
+{
+		_id: Object('...'),
+		name: '',
+		bonus: null,
+		inventory: [
+			{qty:4, id:ObjectId('...'), name: 'potion of healing'},
+			{qty:1, id:ObjectId('...'), name: 'scroll of magic mapping'},
+			{qty:2, id:ObjectId('...'), name: 'c-rations'} ]},	
+		weight: 12,
+		price: 160;
+		...
+}
+
+{
+		_id: Object('...'),
+		name: '',
+		bonus: null,
+		inventory: [
+			{qty:4, id:ObjectId('...'), name: 'potion of healing'},
+			{qty:1, id:ObjectId('...'), name: 'scroll of magic mapping'},
+			{qty:2, id:ObjectId('...'), name: 'c-rations'} ]},	
+		weight: 12,
+		price: 160;
+		...
+}
+
+{
+		_id: Object('...'),
+		name: '',
+		bonus: null,
+		inventory: [
+			{qty:4, id:ObjectId('...'), name: 'potion of healing'},
+			{qty:1, id:ObjectId('...'), name: 'scroll of magic mapping'},
+			{qty:2, id:ObjectId('...'), name: 'c-rations'} ]},	
+		weight: 12,
+		price: 160;
+		...
+}
+
+{
+		_id: Object('...'),
+		name: '',
+		bonus: null,
+		inventory: [
+			{qty:4, id:ObjectId('...'), name: 'potion of healing'},
+			{qty:1, id:ObjectId('...'), name: 'scroll of magic mapping'},
+			{qty:2, id:ObjectId('...'), name: 'c-rations'} ]},	
+		weight: 12,
+		price: 160;
+		...
+}
+
+{
+		_id: Object('...'),
+		name: '',
+		bonus: null,
+		inventory: [
+			{qty:4, id:ObjectId('...'), name: 'potion of healing'},
+			{qty:1, id:ObjectId('...'), name: 'scroll of magic mapping'},
+			{qty:2, id:ObjectId('...'), name: 'c-rations'} ]},	
+		weight: 12,
+		price: 160;
+		...
+}
+
+{
+		_id: Object('...'),
+		name: '',
+		bonus: null,
+		inventory: [
+			{qty:4, id:ObjectId('...'), name: 'potion of healing'},
+			{qty:1, id:ObjectId('...'), name: 'scroll of magic mapping'},
+			{qty:2, id:ObjectId('...'), name: 'c-rations'} ]},	
+		weight: 12,
+		price: 160;
+		...
+}
+
+{
+		_id: Object('...'),
+		name: '',
+		bonus: null,
+		inventory: [
+			{qty:4, id:ObjectId('...'), name: 'potion of healing'},
+			{qty:1, id:ObjectId('...'), name: 'scroll of magic mapping'},
+			{qty:2, id:ObjectId('...'), name: 'c-rations'} ]},	
+		weight: 12,
+		price: 160;
+		...
+}
