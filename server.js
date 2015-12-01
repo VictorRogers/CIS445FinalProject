ef23b54c09aad2647d27e69239451685f16087ed
Victor <victor.r.rogers@gmail.com>
2015-12-01 20:36:19 +0000

    A few additions to server.js not much progress

--- server.js
+++ server.js
@@ -12,38 +12,41 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 mongo.connect("mongodb://localhost:27017/onlineGaming", function(err, db) {
-    if (!err) {
-			console.log("Connected to mongo");
-			// pages
-			app.get('/', function(req, res) {
-				res.render('pages/index', {title:'Index Page'});
+	if (!err) {
+		console.log("Connected to mongo");
+		// pages
+		app.get('/', function(req, res) {
+			//res.render('pages/index', {title:'Index Page'});
+		});
+
+		app.get('/characters', function(req, res) {
+			//res.render('pages/characters', {title:'Character List'});
+			var collection = db.collection('Character');
+			collection.find().toArray(function(err, results) {
+			if (err) throw err;
+				res.json(results);
 			});
+		});
 
-			app.get('/characters', function(req, res) {
-				res.render('pages/characters', {title:'Character List'});
-				var collection = db.collection('Character');
-				collection.find().toArray(function(err, results) {
-				if (err) throw err;
-					res.json(results);
-				});
-			});
+		app.post('/character', function(req, res) {
+      console.log(req.body);
+		});
 
-			app.get('/items', function(req, res) {
-				res.render('pages/items', {title:'Item List'});
-			});
+		app.get('/items', function(req, res) {
+			//res.render('pages/items', {title:'Item List'});
+		});
 
-			app.get('/locations', function(req, res) {
-				res.render('pages/locations', {title:'Location List'});
-			});
+		app.get('/locations', function(req, res) {
+			//res.render('pages/locations', {title:'Location List'});
+		});
 
-			// listen ==========================================================
-			app.listen(port);
-			console.log("App listening on port " + port);
-		}
-		else {
-			console.log(err);
-		}
-
+		// listen ==========================================================
+		app.listen(port);
+		console.log("App listening on port " + port);
+	}
+	else {
+		console.log(err);
+	}
 });
 
 
