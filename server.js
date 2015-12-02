ce27375ddcb9c0766b901c4d16fa913a8cbe288e
Victor <victor.r.rogers@gmail.com>
2015-12-02 02:53:05 +0000

    Added POST functionality to chars, items, locations. Having trouble with passing ObjectIDs for GET, PUT, and DELETE. Will figure out tomorrow after work

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require('express');
 var app			= express();
 var port		= process.env.PORT || 8012;
 var mongo   = require('mongodb').MongoClient;
+var BSON    = require('mongodb').BSONPure;
+var ObjectID = require('mongodb').ObjectID;
 
 // configuration ===================================================
 app.use(express.static(__dirname + '/public'));
@@ -19,26 +21,68 @@ mongo.connect("mongodb://localhost:27017/onlineGaming", function(err, db) {
 			//res.render('pages/index', {title:'Index Page'});
 		});
 
+		//Characters========================================================
 		app.get('/characters', function(req, res) {
 			//res.render('pages/characters', {title:'Character List'});
 			var collection = db.collection('Character');
 			collection.find().toArray(function(err, results) {
-			if (err) throw err;
+				if (err) 
+					throw err;
 				res.json(results);
 			});
 		});
 
-		app.post('/character', function(req, res) {
-      console.log(req.body);
+		app.get('/characters/:id', function (req, res) {
+		  console.log(req.params.id);	
+			var collection = db.collection('Character');
+			_id = new ObjectID(req.params.id);
 		});
 
+    app.post('/addCharacter', function(req, res) {
+			var newCharacter = req.body;
+			var collection = db.collection('Character');
+			collection.insert(newCharacter);
+		});
+		//==================================================================
+
+
+		//Items=============================================================
 		app.get('/items', function(req, res) {
 			//res.render('pages/items', {title:'Item List'});
+			var collection = db.collection('Item');
+			collection.find().toArray(function(err, results) {
+				if (err)
+				  throw err;
+				res.json(results);
+			});
 		});
 
+		app.post('/addItem', function(req, res) {
+			var newItem = req.body;
+			var collection = db.collection('Item');
+			collection.insert(newItem);
+		});
+		//=================================================================
+
+
+    //Locations ========================================================
 		app.get('/locations', function(req, res) {
 			//res.render('pages/locations', {title:'Location List'});
+			var collection = db.collection('Location');
+			collection.find().toArray(function(err, results) {
+				if (err)
+				  throw err;
+			  res.json(results);
+			});
 		});
+
+		app.post('/addLocation', function(req, res) {
+			var newLocation = req.body;
+			var collection = db.collection('Location');
+			collection.insert(newLocation);
+		});
+		//==================================================================
+
 
 		// listen ==========================================================
 		app.listen(port);
